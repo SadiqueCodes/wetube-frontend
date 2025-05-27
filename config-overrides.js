b032ddffa408dc28eda2cb3de14494b6c5f805c8
SadiqueCodes <alisadique800@gmail.com>
2025-05-28 00:35:12 +0530

    Fix: Simplify webpack config and dependencies

--- config-overrides.js
+++ config-overrides.js
@@ -1,23 +1,24 @@
 const webpack = require('webpack');
 
 module.exports = function override(config) {
-  const fallback = config.resolve.fallback || {};
-  Object.assign(fallback, {
-    crypto: require.resolve('crypto-browserify'),
-    stream: require.resolve('stream-browserify'),
-    assert: require.resolve('assert'),
-    http: require.resolve('stream-http'),
-    https: require.resolve('https-browserify'),
-    os: require.resolve('os-browserify'),
-    url: require.resolve('url'),
-    process: require.resolve('process/browser')
-  });
-  config.resolve.fallback = fallback;
-  config.plugins = (config.plugins || []).concat([
+  const fallback = {
+    "process": require.resolve("process/browser")
+  };
+
+  config.resolve = {
+    ...config.resolve,
+    fallback: {
+      ...config.resolve?.fallback,
+      ...fallback,
+    }
+  };
+
+  config.plugins = [
+    ...config.plugins,
     new webpack.ProvidePlugin({
-      process: 'process/browser',
-      Buffer: ['buffer', 'Buffer']
+      process: 'process/browser'
     })
-  ]);
+  ];
+
   return config;
 }; 
